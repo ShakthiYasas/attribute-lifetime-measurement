f9393bc0972a1f9f63c2426d5a04557be32f5c03
Shakthi Weerasinghe <yasas276@gmail.com>
2021-09-24 12:48:32 +1000

    minor fix to greedy lifetime adjustment using freshness

--- coaas-mock-api/profiler.py
+++ coaas-mock-api/profiler.py
@@ -121,8 +121,9 @@ class Profiler:
         total_sum += local_sum
         count += 1
 
-        #Adjusting greedy algorithm to manage with the most expensive SLA
-        if(is_greedy):
+        # Adjusting greedy algorithm to manage with the most expensive SLA
+        # However, considering the lifetime until freshness = 0 if no freshness requirement is yet specificed.
+        if(is_greedy and idx in self.freshness_reqiurement):
             return (total_sum/count)*self.freshness_reqiurement[idx]
 
         return total_sum/count
