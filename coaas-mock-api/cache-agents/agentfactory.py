c6395de7ef785985222d6250d02614735a2a5c50
Shakthi Weerasinghe <yasas276@gmail.com>
2021-10-27 18:33:32 +1100

    minor changes

--- coaas-mock-api/cache-agents/agentfactory.py
+++ coaas-mock-api/cache-agents/agentfactory.py
@@ -2,26 +2,26 @@ from agent import Agent
 from dqnagent import DQNAgent
 from a3cagent import A3CAgent
 
-from configurations.cacheagentconfig import DQNConfiguration, A3CConfiguration
+from configurations.agentconfig import DQNConfiguration, A3CConfiguration
 
 # Instantiate a singleton agent instance according to configuration
 class AgentFactory:
     # Class varaible
-    agent=None
-    configuration = None
+    __agent=None
+    __configuration = None
 
     def __init__(self, configuration = None):
         if(configuration != None):
-            self.configuration = configuration
+            self.__configuration = configuration
 
     # Retruns the singleton instance of a cache
     def get_agent(self, actions, features) -> Agent:
-        if(self.agent == None):
-            if(self.configuration != None and isinstance(self.configuration,DQNConfiguration)):
-                return DQNAgent(actions, features, self.configuration)
-            if(self.configuration != None and isinstance(self.configuration,A3CConfiguration)):
-                return A3CAgent(actions, features, self.configuration)
+        if(self.__agent == None):
+            if(self.__configuration != None and isinstance(self.__configuration,DQNConfiguration)):
+                return DQNAgent(actions, features, self.__configuration)
+            if(self.__configuration != None and isinstance(self.__configuration,A3CConfiguration)):
+                return A3CAgent(actions, features, self.__configuration)
             else:
                 raise ValueError('Invalid configuration.')
         else:
-            return self.agent
+            return self.__agent
---
+++ coaas-mock-api/cache-agents/dqnagent.py
@@ -0,0 +1,6 @@
+from agent import Agent
+
+class DQNAgent(Agent):
+    def __init__(self, n_actions): pass
+    def choose_action(self, observation): pass
+    def store_transition(self, s, a, r, s_): pass
